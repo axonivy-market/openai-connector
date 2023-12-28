ea18c1bd15e2b50fa9deef0f9da757931ca3ebe5
Tu Thanh Nguyen <tu.nguyenthanh@axonactive.com>
2023-12-28 11:12:43 +0700

    Handle feedback

--- openai-connector-demo/src/validators/CommonValidator.java
+++ openai-connector-demo/src/validators/CommonValidator.java
@@ -1,5 +1,8 @@
 package validators;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -11,21 +14,33 @@ import org.apache.commons.lang3.StringUtils;
 
 @FacesValidator(value = "commonValidator")
 public class CommonValidator implements Validator {
+	
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+			Pattern.CASE_INSENSITIVE);
 
 	@Override
 	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		
 		if (isRequiredInputValidator(component, value)) {
-			addGrowlErrorMessage();
+			addGrowlErrorMessage("This field is required");
 		}
 
+		Object inputType = (Object) component.getAttributes().get("type");
+		if (inputType != null && inputType.equals("email") && isValidEmailValidator(component, value)) {
+			addGrowlErrorMessage("Invalid email format");
+		}
 	}
 
 	private boolean isRequiredInputValidator(UIComponent component, Object value) {
 		return value == null || StringUtils.isBlank(value.toString());
 	}
 
-	private void addGrowlErrorMessage() {
-		String message = "This field is required";
+	public static boolean isValidEmailValidator(UIComponent component, Object value) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher((String) value);
+		return !matcher.find();
+	}
+
+	private void addGrowlErrorMessage(String message) {
 		throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
 	}
 }

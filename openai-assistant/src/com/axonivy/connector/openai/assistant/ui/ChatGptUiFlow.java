cfa044512483e7cafd9403a9f0f1372d6503ebe5
Phạm Duy Linh <138570547+linhpd-axonivy@users.noreply.github.com>
2024-08-01 08:59:41 +0700

    MARP-555: add system prompt for edit feature (#69)

--- openai-assistant/src/com/axonivy/connector/openai/assistant/ui/ChatGptUiFlow.java
+++ openai-assistant/src/com/axonivy/connector/openai/assistant/ui/ChatGptUiFlow.java
@@ -63,15 +63,6 @@ public class ChatGptUiFlow {
     var chatGpt = new ChatGptRequest(()->new ChatGptClientFactory().chatGptClient());
     repo.getIntValue(Key.MAX_TOKENS).ifPresent(chatGpt::maxTokens);
 
-    if (quest.equalsIgnoreCase(Quests.EDIT)) {
-      String insert = SwtCommonDialogs.openInputDialog(site.getShell(), "any wishes?", "what can Chat GPT do for you?",
-        "insert a combobox to pick a brand out of: Mercedes, BMW or Tesla");
-      if (insert != null) {
-        var response = runWithProgress(()->chatGpt.edit(what, insert));
-        diffResult(response);
-      }
-      return;
-    }
     if (quest.equalsIgnoreCase(Quests.INSERT)) {
       String insert = SwtCommonDialogs.openInputDialog(site.getShell(), "any wishes?", "what can Chat GPT do for you?",
         "insert a combobox to pick a brand out of: Mercedes, BMW or Tesla");

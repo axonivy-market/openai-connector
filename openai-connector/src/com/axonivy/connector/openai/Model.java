8e000b718f7bb9de993bb4440c26dba025bc9fe8
mnhnam-axonivy <nam.mai@axonactive.com>
2024-10-29 15:16:19 +0700

    IVYPORTAL-17857: Improve-the-OpenAI-Connector - Make the connector supports gpt-4o-mini

--- openai-connector/src/com/axonivy/connector/openai/Model.java
+++ openai-connector/src/com/axonivy/connector/openai/Model.java
@@ -2,8 +2,9 @@ package com.axonivy.connector.openai;
 
 
 public enum Model {
-  GPT_35("gpt-3.5-turbo"),
-  GPT_35_0301("gpt-3.5-turbo-0301");
+  GPT_35("gpt-3.5-turbo"), // deprecated
+  GPT_35_0301("gpt-3.5-turbo-0301"), // deprecated
+  GPT_4O_MINI("gpt-4o-mini");
 
   private final String model;
 

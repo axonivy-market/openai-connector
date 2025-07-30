8e581fd23b314c960896949d27debe02c92dbec5
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 17:14:06 +0700

    pass mvn argurment dirrectly from workflow

--- openai-connector-test/src_test/com/axonivy/connector/openai/constants/OpenAiTestConstants.java
+++ openai-connector-test/src_test/com/axonivy/connector/openai/constants/OpenAiTestConstants.java
@@ -8,5 +8,5 @@ public class OpenAiTestConstants {
   public static final String OPENAI_KEY = "openaiKey";
   public static final String OPEN_AI = "openai";
   public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E Testing";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }

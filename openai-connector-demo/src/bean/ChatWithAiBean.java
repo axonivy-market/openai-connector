e2411de403367869b73208fe700390b5e931a158
ttan-axonivy <an.truongthien@axonactive.com>
2025-08-18 17:48:24 +0700

    MARP-2028 OpenAI connector model parameter (#106)

---
+++ openai-connector-demo/src/bean/ChatWithAiBean.java
@@ -0,0 +1,48 @@
+package bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.openai.api.v1.client.CreateChatCompletionRequest.ModelEnum;
+
+@ManagedBean
+@ViewScoped
+public class ChatWithAiBean {
+
+	private static List<ModelEnum> models;
+	private ModelEnum defaultModel;
+
+	@PostConstruct
+	public void init() {
+		models = List.of(
+		        ModelEnum.GPT_3_5_TURBO_16K,
+		        ModelEnum.GPT_3_5_TURBO_1106,
+		        ModelEnum.GPT_3_5_TURBO_0125,
+		        ModelEnum.GPT_4O,
+		        ModelEnum.GPT_4O_2024_11_20,
+		        ModelEnum.GPT_4O_2024_08_06,
+		        ModelEnum.GPT_4O_2024_05_13,
+		        ModelEnum.CHATGPT_4O_LATEST,
+		        ModelEnum.GPT_4O_MINI,
+		        ModelEnum.GPT_4O_MINI_2024_07_18,
+		        ModelEnum.GPT_4_TURBO,
+		        ModelEnum.GPT_4_TURBO_2024_04_09,
+		        ModelEnum.GPT_4_0125_PREVIEW,
+		        ModelEnum.GPT_4_TURBO_PREVIEW,
+		        ModelEnum.GPT_4_1106_PREVIEW,
+		        ModelEnum.GPT_4
+		    );
+		defaultModel =  ModelEnum.GPT_3_5_TURBO;
+	}
+
+	public List<ModelEnum> getModels() {
+		return models;
+	}
+
+	public ModelEnum getDefaultModel() {
+		return defaultModel;
+	}
+}

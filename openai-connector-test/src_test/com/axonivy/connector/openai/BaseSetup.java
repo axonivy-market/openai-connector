823da04b89b34b623fa46bd3b83fbcc1eb50fcff
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-08-21 16:42:38 +0700

    MARP-2631 Remove unused codes

---
+++ openai-connector-test/src_test/com/axonivy/connector/openai/BaseSetup.java
@@ -0,0 +1,54 @@
+package com.axonivy.connector.openai;
+
+import static com.axonivy.connector.openai.mock.MockAI.URI;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import com.axonivy.connector.openai.constants.OpenAiTestConstants;
+import com.axonivy.utils.e2etest.context.MultiEnvironmentContextProvider;
+import com.axonivy.utils.e2etest.utils.E2ETestUtils;
+
+import ch.ivyteam.ivy.application.IApplication;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.environment.AppFixture;
+import ch.ivyteam.ivy.rest.client.RestClient;
+import ch.ivyteam.ivy.rest.client.RestClients;
+import ch.ivyteam.ivy.rest.client.security.CsrfHeaderFeature;
+
+@IvyProcessTest(enableWebServer = true)
+@ExtendWith(MultiEnvironmentContextProvider.class)
+public class BaseSetup {
+
+  @BeforeEach
+  void beforeEach(ExtensionContext context, AppFixture fixture, IApplication app) {
+    E2ETestUtils.determineConfigForContext(context.getDisplayName(), runRealEnv(fixture), runMockEnv(fixture, app));
+  }
+
+  @AfterEach
+  void afterEach(AppFixture fixture, IApplication app) {
+    RestClients clients = RestClients.of(app);
+    clients.remove(OpenAiTestConstants.OPEN_AI);
+  }
+
+  private Runnable runRealEnv(AppFixture fixture) {
+    return () -> {
+      String openApiKey = System.getProperty(OpenAiTestConstants.OPENAI_KEY);
+      fixture.var("openai-connector.apiKey", openApiKey);
+    };
+  }
+
+  private Runnable runMockEnv(AppFixture fixture, IApplication app) {
+    return () -> {
+      fixture.config("RestClients.openai.Url", URI);
+      fixture.var("openai.apiKey", OpenAiTestConstants.FAKE_KEY);
+      RestClients clients = RestClients.of(app);
+      RestClient openAi = clients.find(OpenAiTestConstants.OPEN_AI);
+      var testClient = openAi.toBuilder().feature(CsrfHeaderFeature.class.getName())
+          .property("AUTH.openaiKey", OpenAiTestConstants.FAKE_KEY).toRestClient();
+      clients.set(testClient);
+    };
+  }
+}
--- openai-connector-test/src_test/com/axonivy/connector/openai/constants/OpenAiTestConstants.java
+++ openai-connector-test/src_test/com/axonivy/connector/openai/constants/OpenAiTestConstants.java
@@ -1,12 +1,8 @@
 package com.axonivy.connector.openai.constants;
 
 public class OpenAiTestConstants {
-  public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
-  public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
   public static final String FAKE_KEY =
       "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTc0MTA3NTYyMSwiaWF0IjoxNzQxMDc1NjIxfQ.8C5qVVQdEArCJmHgeO9pr87rTaZqb5d9fcSa3shecAI";
   public static final String OPENAI_KEY = "openaiKey";
   public static final String OPEN_AI = "openai";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }

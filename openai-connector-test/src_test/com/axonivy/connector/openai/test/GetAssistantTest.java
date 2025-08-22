8536529b1e17f545e4dcdadce53953615d5d1893
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:49:15 +0700

    Feature/marp 2631 unify code for e2e testing approach (#108)

--- openai-connector-test/src_test/com/axonivy/connector/openai/test/GetAssistantTest.java
+++ openai-connector-test/src_test/com/axonivy/connector/openai/test/GetAssistantTest.java
@@ -2,40 +2,18 @@ package com.axonivy.connector.openai.test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
+import com.axonivy.connector.openai.BaseSetup;
 import com.axonivy.connector.openai.constants.OpenAiTestConstants;
-import com.axonivy.connector.openai.context.MultiEnvironmentContextProvider;
 import com.openai.connector.openaiData;
 
-import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
-import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
-import ch.ivyteam.ivy.environment.AppFixture;
-import ch.ivyteam.ivy.rest.client.RestClients;
-import com.axonivy.connector.openai.utils.OpenAiTestUtils;
 
-@IvyProcessTest(enableWebServer = true)
-@ExtendWith(MultiEnvironmentContextProvider.class)
-public class GetAssistantTest {
-
-  @BeforeEach
-  void beforeEach(ExtensionContext context, AppFixture fixture, IApplication app) {
-    OpenAiTestUtils.setUpConfigForContext(context.getDisplayName(), fixture, app);
-  }
-
-  @AfterEach
-  void afterEach(IApplication app) {
-    RestClients clients = RestClients.of(app);
-    clients.remove(OpenAiTestConstants.OPEN_AI);
-  }
+public class GetAssistantTest extends BaseSetup {
 
   @TestTemplate
   public void getAssisstants(BpmClient bpmClient) {
